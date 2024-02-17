7d03e414528bbbe50ed98a91d6859cdb946c17fe
23-dars <23-dars@gmail.com>
2024-02-17 14:36:27 +0500

    Spread operatorlar tugtildi

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 'use strict'
 
+const calc = [1,2,3];
+function logger(x,y,z){
+    console.log(x+y+z);
+}
+
+// logger(1,2,3)
+// logger(calc) <---- ushbu holatda biz aniqlanmagn javobini olamiz
+// Yuqoridagi funksiyni to'g'rilash metodi
+
+logger(...calc); //Massivni SPread operatori yordamida argumentlarga aylantirish
+
+const arr = ['x','t','f'];
+const newArr = [...arr];
+
+// Obe'ktlar bilan ishlash
+
+const numbers = {
+    x : 10,
+    y : 5,
+}
+const  newNumbers = {...numbers}; // yangi obe'ktga ozlashtirish
+
+console.log(numbers);
+console.log(newNumbers);
